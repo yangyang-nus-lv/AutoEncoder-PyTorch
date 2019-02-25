efcc42dbbf60ae1668a18b96c14a8ecd662ae7ed
Yang Yang <yang_yang@u.nus.edu>
2019-02-25 20:40:42 +0800

    interval

--- vae.py
+++ vae.py
@@ -107,7 +107,7 @@ def train(epoch):
         
         optimizer.step()
 
-        if batch_idx % args.log_inteval == 0:
+        if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader),
